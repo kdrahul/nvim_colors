ac94a7b746cbbc13ca49f5bdc69b803d721655fa
Rahul Kulkarni <kdrahul@github.com>
2022-05-28 20:49:45 +0530

    Initial commit with Ayu Mirage

---
+++ README.md
@@ -0,0 +1,12 @@
+# Ayu Mirage for Nvim
+
+Ayu Mirage colors using [tjdevries/colorbuddy.vim](https://github.com/tjdevries/colorbuddy.vim)
+
+## Installation
+
+```vim
+use 'kdrahul/ayu_mirage_nvim'
+
+" And then somewhere in your vimrc, to set the colorscheme
+lua require('colorbuddy').colorscheme('ayu_mirage_nvim')
+```
---
+++ lua/ayu_mirage.lua
@@ -0,0 +1,17 @@
+require("colorbuddy").setup()
+
+local Color = require('colorbuddy').Color
+
+Color.new('foreground',     '#CBCCC5')
+Color.new('background',     '#1F2430')
+Color.new('selection_foreground',     '#1F2430')
+Color.new('selection_background',     '#CBCCC6')
+Color.new('url_cursor',     '#FFCC66')
+Color.new('black',     '#212733')
+Color.new('magenta',     '#EC7171')
+Color.new('red',     '#F08778')
+Color.new('cyan',     '#98E6CA')
+Color.new('green',     '#53BF97')
+Color.new('white',     '#FAFAFA')
+Color.new('yellow',     '#FDCC60')
+Color.new('blue',     '#60B8D6')
